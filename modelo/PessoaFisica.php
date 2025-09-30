13595583c5b8412d99a0471d52e60306177eb63c
Isa Araújo <isabelaa.ifpr2024@gmail.com>
2025-09-30 11:54:56 -0300

    Create PessoaFisica.php

---
+++ modelo/PessoaFisica.php
@@ -0,0 +1,44 @@
+<?php
+
+require_once("modelo/Pessoa.php");
+
+class PessoaFisica extends Pessoa{
+    private int $idade;
+    private string $cpf;
+
+    /**
+     * Get the value of idade
+     */
+    public function getIdade(): int
+    {
+        return $this->idade;
+    }
+
+    /**
+     * Set the value of idade
+     */
+    public function setIdade(int $idade): self
+    {
+        $this->idade = $idade;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of cpf
+     */
+    public function getCpf(): string
+    {
+        return $this->cpf;
+    }
+
+    /**
+     * Set the value of cpf
+     */
+    public function setCpf(string $cpf): self
+    {
+        $this->cpf = $cpf;
+
+        return $this;
+    }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+}
